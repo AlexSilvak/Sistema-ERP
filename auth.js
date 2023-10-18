c35d3e2157b1af876d6f4c84286365e699dd3d1f
Alex Silva <33464413+AlexSilvak@users.noreply.github.com>
2023-10-17 23:31:45 -0300

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,29 @@
+function login(){
+    // validador email   
+   
+   ///<img src="./img/login.png" class="" alt="Responsive image" width="1000" height="935">
+  const email = document.getElementById('floatingEmail')
+   emailValue = email.value;
+   const password= document.getElementById('floatingPassword')
+   passwordValue = password.value; 
+
+   
+   
+   if(emailValue==='alex'){
+    if(passwordValue==='123'){
+      redirectHome()
+   }else{
+    window.alert('Senha Incorreta')
+   }
+   }else{
+    window.alert('Email Incorreto')
+   }
+ 
+  }
+  function redirectHome(){
+    window.open('./home.html')
+   }
+  function logout(){
+    
+    window.open('./index.html')
+    }
